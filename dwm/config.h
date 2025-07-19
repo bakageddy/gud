086c1355fe0dfe72c5efe8e8235fe6d9e9e5b830
bakageddy <gdineshda@gmail.com>
2025-07-20 00:53:38 +0530

    add suckless tools

---
+++ dwm/config.h
@@ -0,0 +1,180 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <X11/XF86keysym.h>
+#include <stddef.h>
+
+/* appearance */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 10;        /* gaps between windows */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "Iosevka NFM:size=10" };
+static const char dmenufont[]       = "Iosevka NFM:size=10";
+
+// Grayscale Dark
+
+static const char grayscale_bg[]       = "#101010";
+static const char grayscale_fg[]       = "#ababab";
+static const char grayscale_gray[]        = "#ababab";
+
+// // Gruvbox
+
+static const char gruvbox_bg[]		= "#1D2021";
+static const char gruvbox_fg[]		= "#D4BE98";
+static const char gruvbox_violet[]		= "#D3869B";
+static const char gruvbox_blue[]		= "#83A598";
+static const char gruvbox_cyan[]		= "#8EC07C";
+static const char gruvbox_green[]		= "#B8BB26";
+static const char gruvbox_yellow[]		= "#FABD2F";
+static const char gruvbox_orange[]		= "#FE8019";
+static const char gruvbox_red[]			= "#FF5F5F";
+
+static const char iceberg_bg[]			= "#161821";
+static const char iceberg_fg[]			= "#c6c8d1";
+static const char iceberg_grey[]		= "#a093c7";
+
+static const char kanso_bg[]			= "#090E13";
+static const char kanso_fg[]			= "#C5C9C7";
+static const char kanso_grey[]			= "#a093c7";
+
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { gruvbox_fg, gruvbox_bg, gruvbox_bg},
+	[SchemeSel]  = { gruvbox_fg, gruvbox_bg, gruvbox_bg},
+};
+
+/* tagging */
+static const char *tags[] = { 
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8"
+};
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+static const Layout layouts[] = {
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+};
+
+/* key definitions */
+#define MODKEY Mod1Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", gruvbox_bg, "-nf", gruvbox_fg, "-sb", gruvbox_green, "-sf", gruvbox_bg, NULL };
+static const char *termcmd[]  = { "st", NULL };
+static const char *altTermCmd[] = { "/bin/sh", "-c", "urxvtc", NULL};
+static const char *yazicmd[] = {"st", "-e", "yazi", NULL};
+static const char *emacscmd[] = {"emacs", "--no-x-resources", NULL};
+static const char *zathuracmd[] = {"zathura", NULL};
+static const char *browsercmd[] = {"firefox", NULL};
+static const char *increase_vol[] = {"amixer", "set", "Master", "5%+", NULL};
+static const char *decrease_vol[] = {"amixer", "set", "Master", "5%-", NULL};
+static const char *mute[] = {"amixer", "set", "Master", "toggle", NULL};
+static const char *screenshotcmd[] = {"/bin/sh", "-c", "scrot ~/Pictures/Screenshots/%Y-%m-%d-%T.png", NULL};
+static const char *decrease_light[] = {"xbacklight", "-5", NULL};
+static const char *increase_light[] = {"xbacklight", "+5", NULL};
+
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY | ShiftMask,           XK_Return, spawn,          {.v = altTermCmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    view,           {0} },
+	{ MODKEY,						XK_w,      killclient,     {0} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	// User defined remaps
+	{ MODKEY | ShiftMask,           XK_b,		spawn,         {.v = browsercmd}},
+	{ MODKEY | ShiftMask,			XK_f,		spawn,		   {.v = yazicmd}},
+	{ MODKEY | ShiftMask,			XK_e,		spawn,		   {.v = emacscmd}},
+	{ MODKEY | ShiftMask,			XK_z,		spawn,		   {.v = zathuracmd}},
+
+	{0,								XF86XK_AudioLowerVolume, spawn, {.v = decrease_vol}},
+	{0,								XF86XK_AudioRaiseVolume, spawn, {.v = increase_vol}},
+	{0,								XF86XK_AudioMute,		 spawn, {.v = mute}},
+	{MODKEY | ShiftMask,			XK_s,					 spawn, {.v = screenshotcmd}},
+	{MODKEY ,				XK_Up,					 spawn, {.v = increase_light}},
+	{MODKEY ,				XK_Down,			     spawn, {.v = decrease_light}},
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
---
+++ slstatus/config.h
@@ -0,0 +1,113 @@
+/* See LICENSE file for copyright and license details. */
+
+/* interval between updates (in ms) */
+const unsigned int interval = 5000;
+
+/* text to show if no value can be retrieved */
+static const char unknown_str[] = "n/a";
+
+/* maximum output string length */
+#define MAXLEN 2048
+#define GRUVBOX_BLACK		"#1d2021"
+#define GRUVBOX_RED			"#ff5f5f"
+#define GRUVBOX_ORANGE		"#fe8019"
+#define GRUVBOX_YELLOW		"#fabd2f"
+#define GRUVBOX_GREEN		"#b8bb26"
+#define GRUVBOX_CYAN		"#8ec07c"
+#define GRUVBOX_BLUE		"#83a598"
+#define GRUVBOX_MAGENTA		"#d3869b"
+#define GRUVBOX_WHITE		"#d4be98"
+
+#define GRAYSCALE_BG		"#101010"
+#define GRAYSCALE FG		"#e3e3e3"
+#define GRAYSCALE_GRAY		"#ababab"
+
+/*
+ * function            description                     argument (example)
+ *
+ * battery_perc        battery percentage              battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * battery_remaining   battery remaining HH:MM         battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * battery_state       battery charging state          battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * cat                 read arbitrary file             path
+ * cpu_freq            cpu frequency in MHz            NULL
+ * cpu_perc            cpu usage in percent            NULL
+ * datetime            date and time                   format string (%F %T)
+ * disk_free           free disk space in GB           mountpoint path (/)
+ * disk_perc           disk usage in percent           mountpoint path (/)
+ * disk_total          total disk space in GB          mountpoint path (/)
+ * disk_used           used disk space in GB           mountpoint path (/)
+ * entropy             available entropy               NULL
+ * gid                 GID of current user             NULL
+ * hostname            hostname                        NULL
+ * ipv4                IPv4 address                    interface name (eth0)
+ * ipv6                IPv6 address                    interface name (eth0)
+ * kernel_release      `uname -r`                      NULL
+ * keyboard_indicators caps/num lock indicators        format string (c?n?)
+ *                                                     see keyboard_indicators.c
+ * keymap              layout (variant) of current     NULL
+ *                     keymap
+ * load_avg            load average                    NULL
+ * netspeed_rx         receive network speed           interface name (wlan0)
+ * netspeed_tx         transfer network speed          interface name (wlan0)
+ * num_files           number of files in a directory  path
+ *                                                     (/home/foo/Inbox/cur)
+ * ram_free            free memory in GB               NULL
+ * ram_perc            memory usage in percent         NULL
+ * ram_total           total memory size in GB         NULL
+ * ram_used            used memory in GB               NULL
+ * run_command         custom shell command            command (echo foo)
+ * swap_free           free swap in GB                 NULL
+ * swap_perc           swap usage in percent           NULL
+ * swap_total          total swap size in GB           NULL
+ * swap_used           used swap in GB                 NULL
+ * temp                temperature in degree celsius   sensor file
+ *                                                     (/sys/class/thermal/...)
+ *                                                     NULL on OpenBSD
+ *                                                     thermal zone on FreeBSD
+ *                                                     (tz0, tz1, etc.)
+ * uid                 UID of current user             NULL
+ * uptime              system uptime                   NULL
+ * username            username of current user        NULL
+ * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * wifi_essid          WiFi ESSID                      interface name (wlan0)
+ * wifi_perc           WiFi signal in percent          interface name (wlan0)
+ */
+
+#define ICEBERG_BLACK	"#161821"
+#define ICEBERG_WHITE	"#c6c8d1"
+#define ICEBERG_RED		"#e27878"
+#define ICEBERG_GREEN	"#b4be82"
+#define ICEBERG_ORANGE	"#e2a478"
+#define ICEBERG_BLUE	"#84a0c6"
+#define ICEBERG_MAGENTA "#a093c7"
+#define ICEBERG_CYAN	"#89b8c2"
+
+#define KANSO_BLACK		"#090E13"
+#define KANSO_WHITE		"#C5C9C7"
+#define KANSO_RED		"#e27878"
+#define KANSO_GREEN		"#b4be82"
+#define KANSO_ORANGE	"#e2a478"
+#define KANSO_BLUE		"#84a0c6"
+#define KANSO_MAGENTA	"#a093c7"
+#define KANSO_CYAN		"#89b8c2"
+
+static const struct arg args[] = {
+    /* function format          argument */
+    {wifi_essid, "^b" GRUVBOX_WHITE "^^c" GRUVBOX_BLACK "^ 󰤥 : %s ^d^ ", "wlan0"},
+    {wifi_perc, "^b" GRUVBOX_RED "^^c" GRUVBOX_BLACK "^ 󰤥 : %s ^d^ ", "wlan0"},
+    {run_command, "^b" GRUVBOX_ORANGE "^^c" GRUVBOX_BLACK "^ 󰕾 : %s ^d^ ",
+     "amixer sget Master | tail -1 | awk '{print $4 }' | sed "
+     "'s@\\(\\[\\|\\]\\)@@g'"},
+
+    {ram_perc, "^b" GRUVBOX_GREEN "^^c" GRUVBOX_BLACK "^ 󰍛 : %s ^d^ ", NULL},
+    {netspeed_rx, "^b" GRUVBOX_CYAN "^^c" GRUVBOX_BLACK "^ Speed: %s ^d^ ", "wlan0"},
+
+    {run_command, "^b" GRUVBOX_BLUE "^^c" GRUVBOX_BLACK "^   %s ^d^ ",
+     "~/.software/scripts/elektric_script"},
+
+    {datetime, "^b" GRUVBOX_MAGENTA "^^c" GRUVBOX_BLACK "^ %s ^d^ ", "%d-%b-%y %H-%M"},
+};
