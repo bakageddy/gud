87fbda3e2c2e46c534f1cb79d911d8dcdabc6908
bakageddy <gdineshda@gmail.com>
2025-09-08 12:27:35 +0530

    use prettierd, niri output

--- nvim/after/plugin/null.lua
+++ nvim/after/plugin/null.lua
@@ -3,8 +3,8 @@ local null = require("null-ls")
 null.setup({
 	sources = {
 		null.builtins.formatting.gofumpt,
-		null.builtins.formatting.sql_formatter
-		-- null.builtins.formatting.prettierd,
+		null.builtins.formatting.sql_formatter,
+		null.builtins.formatting.prettierd,
 		-- null.builtins.formatting.autoflake,
 		--
 		-- null.builtins.diagnostics.revive,
