907b67d0bf0ae93e36fa70608bcc96abec074e9a
bakageddy <gdineshda@gmail.com>
2026-06-17 17:49:20 +0530

    add: zen-browser

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,19 +1,26 @@
 {
-  description = "My Snow Flake";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: 
-  let 
-	pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-	  devShells.x86_64-linux.default = pkgs.mkShell {
-		packages = with pkgs; [
-			ripgrep
-			fd
-			neovim
-		];
-	  };
-  };
+	description = "My Snow Flake with homemanager";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";                                                                                                                      
+		};
+		zen-browser = {
+			url = "github:youwen5/zen-browser-flake";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+	outputs = { self, nixpkgs, home-manager }:
+	let 
+		pkgs = nixpkgs.legacyPackages.x86_64-linux;
+		username = "dinesh-24010";
+		system = "x86_64-linux";
+	in {
+		homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+            pkgs = nixpkgs.legacyPackages.${system};                                                                                                               
+            extraSpecialArgs = { inherit system username zen-browser; };                                                                                                       
+            modules = [ ./home.nix ];                                                                                                                              
+        };
+	};
 }
--- nix/home.nix
+++ nix/home.nix
@@ -1,14 +1,14 @@
-{ pkgs }:
+{ pkgs, zen-browser, ... }:
 {                                                                                                                                                              
 	home.username = "dinesh-24010"; # CHANGE THIS TO YOUR USERNAME                                                                                               
 	home.homeDirectory = "/home/dinesh-24010"; # CHANGE THIS TO YOUR HOME DIRECTORY                                                                              
 																																								 
 	home.packages = with pkgs; [                                                                                                                                 
-	  ripgrep                                                                                                                                                    
-	  fd                                                                                                                                                         
+		ripgrep
+		fd
+		htop
+		zen-browser.packages.x86_64-linux.default
 	];                                                                                                                                                           
-																																								 
 	programs.home-manager.enable = true;                                                                                                                         
-																																								 
 	home.stateVersion = "23.11";                                                                                                                                 
 }     
