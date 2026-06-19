1c35ffca36daa987a2a27aad3a82683e29e26a36
bakageddy <gdineshda@gmail.com>
2026-06-19 16:35:12 +0530

    add turso,sqlite,bruno

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,26 +1,33 @@
 {
-	description = "My Snow Flake with homemanager";
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-		home-manager = {
-			url = "github:nix-community/home-manager";
-			inputs.nixpkgs.follows = "nixpkgs";                                                                                                                      
-		};
-		zen-browser = {
-			url = "github:youwen5/zen-browser-flake";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
-	outputs = { self, nixpkgs, home-manager, zen-browser }:
-	let 
-		pkgs = nixpkgs.legacyPackages.x86_64-linux;
-		username = "dinesh-24010";
-		system = "x86_64-linux";
-	in {
-		homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-            pkgs = nixpkgs.legacyPackages.${system};                                                                                                               
-            extraSpecialArgs = { inherit system username zen-browser; };                                                                                                       
-            modules = [ ./home.nix ];                                                                                                                              
-        };
-	};
+  description = "My Snow Flake with homemanager";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      zen-browser,
+    }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      username = "dinesh-24010";
+      system = "x86_64-linux";
+    in
+    {
+      homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        extraSpecialArgs = { inherit system username zen-browser; };
+        modules = [ ./home.nix ];
+      };
+    };
 }
--- nix/home.nix
+++ nix/home.nix
@@ -1,25 +1,64 @@
-{ pkgs, zen-browser, ... }:
-{                                                                                                                                                              
-	home.username = "dinesh-24010"; # CHANGE THIS TO YOUR USERNAME                                                                                               
-	home.homeDirectory = "/home/dinesh-24010"; # CHANGE THIS TO YOUR HOME DIRECTORY                                                                              
-																																								 
-	home.packages = with pkgs; [                                                                                                                                 
-		ripgrep
-		fd
-		htop
-		ungoogled-chromium
-		zen-browser.packages.x86_64-linux.default
-		foot
-		ghostty
-		go
-		zig
-		deno
-		nodejs
-		pnpm
-		openjdk
-		jre
-		scala
-	];                                                                                                                                                           
-	programs.home-manager.enable = true;                                                                                                                         
-	home.stateVersion = "23.11";                                                                                                                                 
-}     
+{
+  pkgs,
+  zen-browser,
+  programs,
+  ...
+}:
+{
+  home.username = "dinesh-24010"; # CHANGE THIS TO YOUR USERNAME
+  home.homeDirectory = "/home/dinesh-24010"; # CHANGE THIS TO YOUR HOME DIRECTORY
+  programs.chromium = {
+    enable = true;
+    package = pkgs.ungoogled-chromium;
+    commandLineArgs = [
+      "--no-sandbox"
+      "--enable-features=UseOzonePlatform"
+      "--ozone-platform=wayland"
+    ];
+  };
+
+  programs.bruno = {
+    enable = true;
+    package = pkgs.bruno;
+    commandLineArgs = [
+      "--no-sandbox"
+      "--enable-features=UseOzonePlatform"
+      "--ozone-platform=wayland"
+    ];
+  };
+
+  home.packages = with pkgs; [
+
+    thunar
+    htop
+    zen-browser.packages.x86_64-linux.default
+
+    go
+    zig
+
+    deno
+    nodejs
+    pnpm
+
+    openjdk
+    jre
+    scala
+    eclipse-mat
+
+    zathura
+    zathuraPkgs.zathura_pdf_mupdf
+    zathuraPkgs.zathura_djvu
+
+    foot
+    ghostty
+
+    # bruno-cli
+    duckdb
+    sqlite
+    turso
+    ripgrep
+    fd
+  ];
+  programs.home-manager.enable = true;
+  home.stateVersion = "23.11";
+}
